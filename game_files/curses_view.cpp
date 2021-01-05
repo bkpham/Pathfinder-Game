e118ec440821c700868f0974171209735bd61862
bkpham <60329930+bkpham@users.noreply.github.com>
2021-01-04 17:59:59 -0800

    Update and rename curses_view.cpp to game_files/curses_view.cpp

--- game_files/curses_view.cpp
+++ game_files/curses_view.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 #include "curses_view.hpp"
 
-/**
- * Side note: This class should probably be what is known as a singleton
- * class, i.e. a class that uses some setup (e.g. a static variable) to
- * prevent more than one instance of it from being created. I did not do this.
- */
 
 CursesView::CursesView(int height, int width)
     : View(height, width)
@@ -14,13 +9,10 @@ CursesView::CursesView(int height, int width)
     cbreak();
     noecho();
 
-    // TODO: Implement. Set up curses.
-
 }
 
 CursesView::~CursesView()
 {
-    // TODO: Implement. Clean up curses.
     endwin();
 }
 
@@ -35,8 +27,4 @@ void CursesView::draw(const std::vector<std::string>& lines)
     refresh();
     curs_set(0);
     refresh();
-
-
-    // TODO: Implement.
-
 }
