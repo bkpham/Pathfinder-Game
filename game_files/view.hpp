4374bd98dfbda4e6018212e88353c4d16885d57d
bkpham <60329930+bkpham@users.noreply.github.com>
2021-01-04 18:09:30 -0800

    Update and rename view.hpp to game_files/view.hpp

--- game_files/view.hpp
+++ game_files/view.hpp
@@ -4,11 +4,6 @@
 #include <string>
 #include <vector>
 
-/**
- * I don't think you will have any reason to modify this file
- * or view.cpp.
- */
-
 class View
 {
 public:
@@ -20,11 +15,6 @@ public:
 
     int getHeight() const { return mHeight; }
     int getWidth() const { return mWidth; }
-
-    /**
-     * Output each line of the given vector.
-     * What exactly is meant by "output" is up to the subclass.
-     */
     virtual void draw(const std::vector<std::string>& lines) = 0;
 
 protected:
