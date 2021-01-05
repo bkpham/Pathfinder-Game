7243a53101f8f576d5c9b37d0d383c52f49f587b
bkpham <60329930+bkpham@users.noreply.github.com>
2021-01-04 18:03:33 -0800

    Update and rename game.hpp to game_files/game.hpp

--- game_files/game.hpp
+++ game_files/game.hpp
@@ -38,55 +38,18 @@ private:
 
 class Game
 {
-public:  // public methods
-    /**
-     * DO NOT change the prototype of this function.
-     * The autograder WILL call this function.
-     */
+public:  
     Game(const std::string& filename, InterfaceType interfaceType);
 
     ~Game();
     Game(const Game&) = delete;
     Game& operator=(const Game&) = delete;
-    /**
-     * DO NOT change the prototype of this function.
-     * The autograder WILL call this function.
-     */
     void run();
 
-private:  // private methods
-    /**
-     * You DO NOT need to have any of the private methods below.
-     * I just left some commented out declarations in case it
-     * helps guide your thinking, but you don't need to use them.
-     */
-
-    /**
-     * Set up the 2D vector of strings that is then given to the draw()
-     * method of an instance of a subclass of View.
-     */
+private:  
     std::vector<std::string> draw( MapSegment& map);
-    //given a map segment, checks if moving forward will result in collision
+    
     int noCollision( MapSegment& map);
-
-
-
-    /**
-     * Loop until the game is over. On each iteration, use the controller
-     * to get input from the user.
-     */
-    // void doGameLoop();
-
-    /**
-     * Update matters related to game logic, e.g. collisions, level completion.
-     */
-    // void update();
-
-    /**
-     * Load information related to new level into member variables,
-     * being careful to not leave traces of information from previous levels. 
-     */
-    // void loadLevel();
 
     static const int MIN_VIEW_HEIGHT;
     static const int MIN_VIEW_WIDTH;
@@ -106,15 +69,6 @@ private:  // private methods
     int items;
     int map_segment;
 
-
-
-    /**
-     * Because one of the constructor parameters must be checked in order
-     * to determine the subclass types for the below members, they can't
-     * be references, because there's no default constructors for them.
-     * (Recall that each member in a class must either be initialized by an
-     * initializer list in the constructor OR have a default constructor.)
-     */
     View* mView;
     Controller* mController;
 
