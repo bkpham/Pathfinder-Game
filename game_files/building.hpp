09fc03b2845183e7f104eda97e5a81275053a274
bkpham <60329930+bkpham@users.noreply.github.com>
2021-01-04 17:57:48 -0800

    Update building.hpp

--- game_files/building.hpp
+++ game_files/building.hpp
@@ -8,23 +8,7 @@ class Building
 {
 public:
     Building(int y, int x);
-
-    /**
-     * You DO NOT have to have either of the below methods.
-     * I leave them as suggestions.
-     */
-
-    /**
-     * Draws self onto the given board.
-     */
     void drawIn(std::vector<std::string>& board) const;
-
-    /**
-     * Returns true if the given coordinates are contained
-     * in this building.
-     */
-    // bool contains(int y, int x) const;
-
 private:
     static const int width;
     static const int height;
